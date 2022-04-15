380fb8297d392b3e927484ea7ea2582958b0b675
yashwable <yashwable12@gmail.com>
2022-04-15 15:30:55 +0530

    tut 4

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/order');
 
+app.use(morgan('dev'));
 
+//adding changes
 app.use('/products' , productRoutes) ;
 app.use('/order' , orderRoutes) ;
+
+app.use ((req,res,next)=> {
+    const error = new Error('not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error,req,res,next) => {
+    res.status(error.status || 500);
+    res.json({
+        error :{
+            message : error.message
+        }
+    });
+});
+
+
 
 module.exports = app ;
