e25997e66c129e0bfcd71eb0b2cd3911451c1278
yashwable <yashwable12@gmail.com>
2022-04-15 23:36:24 +0530

    tut 5

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,13 @@
  });
 
  router.post ('/', (req, res ,next) => {
+     const product = {
+        name :req.body.name,
+        price : req.body.price
+     }
     res.status(200).json({
-        message: 'Handling POST requst to /products'
+        message: 'Handling POST requst to /products',
+        createdProduct : product
     });
 });
 
--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/order');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-control-Allow-Headers','Origin,X-Requested-With,content-Type,Accept,Authorisation');
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,PATCH');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //adding changes
 app.use('/products' , productRoutes) ;
