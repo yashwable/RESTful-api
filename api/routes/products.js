7663c750fbbc8b964f7da606c3348629ce9435e3
yashwable <yashwable12@gmail.com>
2022-04-22 20:59:10 +0530

    tut 10

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,7 +3,18 @@
  const Product = require('../models/product');
  const mongoose = require ('mongoose');
  const multer = require('multer');
- const upload = multer({dest : 'uploads/'})
+ const path = require ('path');
+
+const storage = multer.diskStorage({
+    destination : (req , file , cb) => {
+        cb (null , 'uploads/');
+    },
+    filename : (req , file , cb) => {
+        cb (null , Date.now() + path.extname(file.originalname));
+    }
+});
+
+ const upload = multer({storage : storage});
 
 
  router.get ('/', (req, res ,next) => {
@@ -36,7 +47,7 @@
      });
  });
 
- router.post ('/',upload.single ('productImage'), (req, res ,next) => {
+ router.post ('/',upload.single('productImage'), (req, res ,next) => {
      console.log(req.file)
      const product = new Product({
          _id : new mongoose.Types.ObjectId(),
