37781bf765a047ca0a95729f9815b4cd1600f6a4
yashwable <yashwable12@gmail.com>
2022-04-28 19:04:01 +0530

    tut 12.1

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require ("express");
 const router = express.Router();
 const mongoose = require ('mongoose');
 const bcrypt = require ('bcrypt');
+const jwt = require ('jsonwebtoken')
 
 const User = require ("../models/user");
 const user = require("../models/user");
@@ -62,8 +63,19 @@ router.post("/login",(req,res,next) => {
                 });
             }
             if (result){
+                const token = jwt.sign(
+                    {
+                        email : user[0].email,
+                        userId : user[0]._id
+                    },
+                    'secret',
+                    {
+                        expiresIn : "1h"
+                    }
+                    );
                 return res.status(200).json({
-                    message:'Auth successful'
+                    message:'Auth successful',
+                    token:token
                 });
             }
             res.status(401).json({
